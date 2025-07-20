72aade1d430931ba0c2d022c8c346cc4193c2727
Ushosi Pal <ushosi2001@gmail.com>
2025-07-20 19:50:34 +0530

    Add files via upload

---
+++ A.java
@@ -0,0 +1,10 @@
+import java.util.scanner
+class A{
+    public static void main(String []args){
+ scanner sc= new scanner(System.in);
+ System.out.println("Enter the number: ");
+ int num=scanner.nextInt();
+ int factorial=fact(num);
+ 
+    }
+}
